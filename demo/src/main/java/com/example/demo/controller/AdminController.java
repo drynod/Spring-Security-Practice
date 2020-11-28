629c0eae8fa5b9c9c614f2603e27cdc35178827b
boxlog <esesmineun3@gmail.com>
2020-11-29 00:03:20 +0900

    어드민 멤버 회원 조회 컨트롤러 추가

--- demo/src/main/java/com/example/demo/controller/AdminController.java
+++ demo/src/main/java/com/example/demo/controller/AdminController.java
@@ -1,12 +1,34 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.Member;
+import com.example.demo.service.MemberService;
+import com.example.demo.service.RoleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final MemberService memberService;
+    private final RoleService roleService;
+
+
     @GetMapping("/admin")
     public String config() throws Exception{
         return "admin/config";
     }
+
+    @GetMapping("/admin/members")
+    public String getMembers(Model model) throws Exception{
+        List<Member> members = memberService.getMembers();
+        model.addAttribute("members", members);
+
+        return "admin/member/list";
+    }
+
 }

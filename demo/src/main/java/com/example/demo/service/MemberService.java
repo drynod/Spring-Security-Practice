7f7637715b0d170597fbc431b4528fc283fe99b3
boxlog <esesmineun3@gmail.com>
2020-11-28 23:58:23 +0900

    회원 서비스 수정

--- demo/src/main/java/com/example/demo/service/MemberService.java
+++ demo/src/main/java/com/example/demo/service/MemberService.java
@@ -2,10 +2,22 @@ package com.example.demo.service;
 
 
 import com.example.demo.domain.Member;
+import com.example.demo.dto.MemberDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 public interface MemberService {
     void createUser(Member member);
 
+    void modifyUser(MemberDto memberDto);
+
+    List<Member> getMembers();
+
+    void deleteMembers(Long id);
+
+    MemberDto getMember(Long id);
+
+    void orders();
 }
